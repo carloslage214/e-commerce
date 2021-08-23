041900b9c791cf27c50616ba482e3772ed39c20a
carloslage214 <carlos-411@hotmail.com>
2021-08-22 22:06:29 -0300

    E1

--- js/products.js
+++ js/products.js
@@ -1,6 +1,45 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
 
+document.addEventListener("DOMContentLoaded", function () {
+    getProducts(PRODUCTS_URL)
 });
+
+async function getProducts(url) {
+    let resultObj = await getJSONData(url);
+    if (resultObj.status === 'ok') {
+        showProductsList(resultObj.data);
+    } else {
+        console.log(resultObj.status);
+    }
+}
+
+
+function showProductsList(resultObj){
+
+    let htmlContentToAppend = "";
+    for(let i = 0; i < resultObj.length; i++){
+        let product = resultObj[i];
+
+            htmlContentToAppend += `
+            <a href="product-info.html" class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">`+ product.name +`</h4>
+                            <small class="text-muted">` + product.soldCount + ` artículos</small>
+                        </div>
+                        <p class="mb-1">` + product.description + `</p>
+                    </div>
+                </div>
+            </a>
+            `
+        }
+
+        document.getElementById("product-list-container").innerHTML = htmlContentToAppend;
+    }
+
